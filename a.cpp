907aafaeab1c142046932abb82a664cba4ef18fd
Vaibhav Saxena <Vaibhav.1865@gmail.com>
2022-08-06 11:45:03 +0530

    Added a.cpp and readme

---
+++ a.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+
+    cout<<"Enter the value of a: ";
+    cin>>a;
+    cout<<"Enter the value of b: ";
+    cin>>b;
+    cout<<"Enter the value of c: ";
+    cin>>c;
+    cout<<"The values are  "<<a<<" "<<b<<" "<<c<<endl;
+    return 0;
+    
+}
